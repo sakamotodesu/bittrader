b09f158c581ddd49de52daaef87bbea01117fc37
Minoru Sakamoto <p2020640@gmail.com>
2020-05-09 19:24:41 +0900

    #17 ECSを追加

--- tf/main.tf
+++ tf/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sakamotodesu-bittrader-tfstate"
-    key    = "terraform.tfstate"
+    key = "terraform.tfstate"
     region = "ap-northeast-1"
   }
   required_version = "= 0.12.23"
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "bittrader" {
-  name                 = "bittrader"
+  name = "bittrader"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -51,4 +51,42 @@ resource "aws_ecr_repository_policy" "bittrader-policy" {
     ]
 }
 EOF
+}
+
+resource "aws_ecs_cluster" "bittrader-cluster" {
+  name = "bittrader-cluster"
+}
+
+resource "aws_ecs_service" "bittrader-service" {
+  name = "bittrader-service"
+  cluster = aws_ecs_cluster.bittrader-cluster.id
+  task_definition = aws_ecs_task_definition.bittrader-service.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  platform_version = "1.3.0"
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups = [
+      "sg-0577bab4e1fc38cbf"]
+    subnets = [
+      "subnet-02902f75"
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      task_definition]
+  }
+}
+
+resource "aws_ecs_task_definition" "bittrader-service" {
+  family = "bittrader-service"
+  cpu = 256
+  memory = 512
+  network_mode = "awsvpc"
+  execution_role_arn = "arn:aws:iam::616703994274:role/ecsTaskExecutionRole"
+  requires_compatibilities = [
+    "FARGATE"]
+  container_definitions = file("task-definitions/bittrader-service.json")
 }
