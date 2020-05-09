1d21afde226eceea0b42ef8213c24b1b212c2cfe
Minoru Sakamoto <p2020640@gmail.com>
2020-05-09 14:43:57 +0900

    #17 ECRを追加

--- tf/main.tf
+++ tf/main.tf
@@ -10,3 +10,45 @@ terraform {
 provider "aws" {
   version = "= 2.52.0"
 }
+
+resource "aws_ecr_repository" "bittrader" {
+  name                 = "bittrader"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository_policy" "bittrader-policy" {
+  repository = aws_ecr_repository.bittrader.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "circleci_allow",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
